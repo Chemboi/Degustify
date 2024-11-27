9f5e16736e5c3df3d73221f81b534d377775b9cb
Jonathan GA <jga.sub@hotmail.com>
2024-11-27 16:18:25 -0600

    Updated layout for greetings section, added French translations, and new images.

--- static/js/lang.js
+++ static/js/lang.js
@@ -1,18 +1,89 @@
 const translations = {
+    
     en: {
+        
+        /* header */ 
         title: "Degustify",
-        description: "Discover the best wines from around the world.",
+
         redWine: "Red Wine",
+
         whiteWine: "White Wine",
+
         vineyards: "Vineyards",
+
         contact: "Contact Us",
+
+        login: "Login",
+
+
+        /* 1st section */
+        description: "Discover the best wines from Quebec.",
+
+        additionalInfo: "Discover the unique offerings of Quebec's wineries with curated insights, expert reviews, and thoughtful pairing suggestions designed for enthusiasts and curious beginners alike. From uncovering hidden gems to savoring Quebec's finest selections, embark on a tasting journey that celebrates the heart of Quebec's wine culture.",
+
+
+        /* 2nd section */
+        aboutTitle:"More about Degustify",
+
+        aboutDescription: "At Degustify, we are passionate about showcasing the exceptional wines from Quebec's vineyards, from affordable treasures to premium selections available at SAQ stores. Our mission is to deepen your appreciation for Quebec's rich winemaking heritage by providing curated insights, expert reviews, and thoughtful pairing recommendations.Discover the stories behind Quebec's finest bottles, learn about the region's hidden gems, and explore a platform crafted for both wine enthusiasts and curious beginners.",
+
+        subscribeTitle: "Subscribe",
+
+        subscribeDescription: "Sign up to our newsletter with your email to receive exclusive insights into Quebec's wineries, wine reviews, pairing tips, and updates on the finest selections at SAQ. Be the first to uncover hidden gems, savor new flavors, and celebrate Quebec's vibrant wine culture and all delivered straight to your inbox.",
+
+        subscribeButton: "Sign up",
+
+        placeholderFirstName: "First Name",
+
+        placeholderLastName: "Last Name",
+
+        placeholderEmail: "Email Address",
+
+        explore: "Explore",
+
+        followUs: "Follow Us",
     },
+
+
     fr: {
+
+        /* header */ 
         title: "Degustify",
-        description: "Découvrez les meilleurs vins du monde entier.",
+    
         redWine: "Vin Rouge",
+    
         whiteWine: "Vin Blanc",
+    
         vineyards: "Vignobles",
+    
         contact: "Contactez-nous",
-    },
+    
+        login: "Connectez-vous",
+    
+        /* 1st section */
+        description: "Découvrez les meilleurs vins du Québec.",
+    
+        additionalInfo: "Découvrez les offres uniques des vignobles du Québec grâce à des informations sélectionnées, des critiques d'experts et des suggestions d'accords réfléchies conçues pour les passionnés et les curieux. De la découverte de trésors cachés à la dégustation des meilleures sélections du Québec, embarquez pour un voyage gustatif qui célèbre l'essence de la culture vinicole québécoise.",
+    
+        /* 2nd section */
+        aboutTitle: "À propos de Degustify",
+    
+        aboutDescription: "Chez Degustify, nous sommes passionnés par la mise en valeur des vins exceptionnels des vignobles du Québec, allant de trésors abordables à des sélections premium disponibles dans les magasins SAQ. Notre mission est de renforcer votre appréciation de l'héritage vinicole riche du Québec en fournissant des informations sélectionnées, des critiques d'experts et des recommandations d'accord réfléchies. Découvrez les histoires derrière les meilleures bouteilles du Québec, apprenez-en davantage sur les trésors cachés de la région et explorez une plateforme conçue pour les amateurs de vin et les curieux.",
+    
+        subscribeTitle: "S'inscrire",
+    
+        subscribeDescription: "Inscrivez-vous à notre infolettre avec votre courriel pour recevoir des informations exclusives sur les vignobles du Québec, des critiques de vins, des conseils d'accords et des mises à jour sur les meilleures sélections de la SAQ. Soyez le premier à découvrir des trésors cachés, savourer de nouvelles saveurs et célébrer la culture vinicole vibrante du Québec, le tout livré directement dans votre boîte de réception.",
+    
+        subscribeButton: "S'inscrire",
+    
+        placeholderFirstName: "Prénom",
+    
+        placeholderLastName: "Nom de famille",
+    
+        placeholderEmail: "Adresse e-mail",
+    
+        explore: "Explorer",
+    
+        followUs: "Suivez-nous",
+    }
 };
